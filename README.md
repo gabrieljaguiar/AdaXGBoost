5a2c18a242131f5f6515b3cbf4d771b7080b9931
Gabriel Jonas <gabriel.jonas77@gmail.com>
2020-12-21 11:37:58 -0300

    skeleton

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # AdaXGBoost
+
+This is the implementation of the Adaptive XGBoost classifier as described in this paper:
+
+> Montiel, Jacob, Mitchell, Rory, Frank, Eibe, Pfahringer, Bernhard,
+> Abdessalem, Talel, and Bifet, Albert. “AdaptiveXGBoost for Evolving
+> Data Streams”. In:IJCNN’20. International Joint Conference on Neural
+> Networks. 2020. Forthcoming.
+
+This implementation is written in Java and built on top of
+[Massive Online Analysis (MOA)](https://github.com/Waikato/moa) framework.
--- adaptativeXGBoost/AdaptativeXGBoost.java
+++ adaptativeXGBoost/AdaptativeXGBoost.java
@@ -1,11 +1,13 @@
 package moa.classifiers;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Random;
 
 import com.github.javacliparser.FlagOption;
 import com.github.javacliparser.FloatOption;
 import com.github.javacliparser.IntOption;
+import com.github.javacliparser.StringOption;
 
 import com.yahoo.labs.samoa.instances.Instance;
 import com.yahoo.labs.samoa.instances.Instances;
@@ -15,32 +17,63 @@ import moa.classifiers.core.splitcriteria.SplitCriterion;
 import moa.core.Measurement;
 import moa.options.ClassOption;
 
-//import ml.dmlc.xgboost4j.java.DMatrix;
-//import ml.dmlc.xgboost4j.java.Booster;
-//import ml.dmlc.xgboost4j.java.XGBoost;
+import ml.dmlc.xgboost4j.java.DMatrix;
+import ml.dmlc.xgboost4j.java.Booster;
+import ml.dmlc.xgboost4j.java.XGBoost;
 
-public class AdaptativeXGBoost extends AbstractClassifier implements MultiClassClassifier{
+public class AdaptativeXGBoost extends AbstractClassifier {
 	private static final long serialVersionUID = 1L;
 	
+    
+    public IntOption nEstimators = new IntOption("Number of Estimators", 'n', "Number of Estimators", 5, 1, Integer.MAX_VALUE);
+
+    public FloatOption learningRate = new FloatOption("Learning Rate", 'l', 'Learning Rate', 0.3, 0, Float.MAX_VALUES);
+
+    public IntOption maxDepth = new IntOption("Max Depth", 'p', "Max Depth", 6, 1, Integer.MAX_VALUE);
+
+    public IntOption maxWindowSize = new IntOption("Max windowSize", 'm', "Max Window size", 1000, 1, Integer.MAX_VALUE);
+
+    public IntOption minWindowSize = new IntOption("Min WindowSize", 'w', "Min Window size", null, 1, Integer.MAX_VALUE);
+
+    public FlagOption detectDrift = new FlagOption("Detect Drift", 'd', "Detect Drift Flag");
+
+    public StringOption uptadeStrategy = new StringOption ("Update Strategy", 'u', "Uptade Strategy", "replace");
+    
+
+
     protected Instances window;
 
-    // protected XGBoost[] ensemble;
+    protected List<XGBoost> ensemble;
 
-    public IntOption windowSize = new IntOption("windowSize", 's', "Window size", 1000, 1, Integer.MAX_VALUE);
+    protected Integer windowSize;
+
+    protected Integer dynamicWindowSize;
     
-    public IntOption gracePeriodOption = new IntOption("gracePeriod", 'g',
-			"The number of instances to observe between model changes.",
-			1000, 0, Integer.MAX_VALUE);
+    protected Float initMargin;
 
-	public FlagOption binarySplitsOption = new FlagOption("binarySplits", 'b',
-			"Only allow binary splits.");
+    protected Integer modelIndex; 
 
-	public ClassOption splitCriterionOption = new ClassOption("splitCriterion",
-			'c', "Split criterion to use.", SplitCriterion.class,
-			"InfoGainSplitCriterion");
+    protected Integer samplesSeen;
+
+    private PUSH_STRATEGY = "push";
+
+    private REPLACE_STRATEGY = "replace";
+
     
+
+    /* Constructor */
     @Override
     public void resetLearningImpl() {
+        this.ensemble = new ArrayList<XGBoost>();
+        if (this.uptadeStrategy.getValue() == this.REPLACE_STRATEGY){
+            for (int i = 0; i < this.nEstimators; i++){
+                this.ensemble.add(null);
+            }
+        } 
+        this.resetWindowSize();
+        this.initMargin = 0.0;
+        this.modelIndex = 0;
+        this.samplesSeen = 0;
     }
 
     /* Predict method */
@@ -62,13 +95,20 @@ public class AdaptativeXGBoost extends AbstractClassifier implements MultiClassC
 
         System.out.println("Training...");
 
-        if (window == null)
-            window = new Instances(instance.dataset(), 0);
-
         window.add(instance);
 
-        if (window.size() == windowSize.getValue()) {
-            window.delete();
+        while (window.size() >= this.windowSize.getValue()) {
+            this.trainOnMiniBatch(window) // Needs to be improved. Select just 1 to WindowSize Instances;
+            int i = 0;
+            while (i < this.windowSize.getValue()){
+                window.delete(0);
+                i++;
+            }
+            this.adjustWindowSize();
+            if (this.detectDrift.getValue()){
+                System.out.println("Drift");
+                /* To do*/
+            }
         }
     }
 
@@ -86,5 +126,62 @@ public class AdaptativeXGBoost extends AbstractClassifier implements MultiClassC
     
     public boolean isRandomizable() {
         return true;
+    }
+
+    private trainOnMiniBatch(Instances data){
+        if (this.uptadeStrategy.getValue() == this.REPLACE_STRATEGY){
+            XGBoost booster = this.trainBooster(data, this.modelIndex);
+            this.ensemble.set(this.modelIndex, booster);
+            this.samplesSeen = this.samplesSeen + data.getSize();
+            this.updateModelId();
+        }else{
+            XGBoost booster = this.trainBooster(data, this.ensemble.size());
+            if (this.ensemble.size() == this.nEstimators.getValue){
+                this.ensemble.remove(0);
+            }
+            this.ensemble.add(booster);
+            this.samplesSeen = this.samplesSeen + data.getSize();
+
+        }
+    }
+
+    private trainBooster(Intances data, Integer len){
+        /* To be implemented */
+        return True;
+    }
+
+    private updateModelId(){
+        this.modelIndex++;
+        if (this.modelIndex == this.nEstimators.getValue()){
+            this.modelIndex = 0;
+        }
+    }
+
+
+
+    private resetWindowSize(){
+        if (this.minWindowSize){
+            this.dynamicWindowSize = this.minWindowSize.getValue();
+        }else{
+            this.dynamicWindowSize = this.maxWindowSize.getValue();
+        }
+        this.windowSize = this.dynamicWindowSize;
+    }
+
+    private adjustWindowSize(){
+        if(this.dynamicWindowSize < this.maxWindowSize.getValue()){
+            this.dynamicWindowSize = this.dynamicWindowSize * 2;
+            if (this.dynamicWindowSize > this.maxWindowSize.getValue()){
+                this.windowSize = this.maxWindowSize.getValue();
+            }else{
+                this.windowSize = this.dynamicWindowSize;
+            }
+        }
+    }
+
+
+    /* Main just for tests and debugging, remove in final version */
+    public static void main(String[] args) {
+        
     }
 }
